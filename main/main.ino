b8b520ecd34d925e998b1d1cbc02588d4743988d
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-10-03 21:01:13 +0200

    random pattern of leds

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,7 @@
+#include<stdlib.h>
+int score;
+int Factor;
+int* pattern;
 #define L1 2
 #define L2 3
 #define L3 4
@@ -11,23 +15,34 @@
 #define T2 9
 #define T3 10
 #define T4 11
-int counter;
-int roundabout;
+//int counter;
+//int roundabout;
 void setup() {
+  Serial.begin(9600);
   pinMode(L1,OUTPUT);
   pinMode(L2,OUTPUT);
   pinMode(L3,OUTPUT);
   pinMode(L4,OUTPUT);
   pinMode(Ls,OUTPUT);
-  counter = 0;
-  roundabout = 1;
+  score = 0;
+  Factor = 1;
+  pattern = (int *)malloc(sizeof(int) * Factor); 
+  //counter = 0;
+  //roundabout = 1;
 }
 
 void loop() {
-  test();
+  //test();
+  pattern();
 }
-
-void test(){
+void pattern(){
+  int value = random(4);
+  //Serial.println(value);
+  digitalWrite(value+2, HIGH);
+  delay(200);
+  digitalWrite(value+2, LOW);
+}
+/*void test(){
   counter=counter+roundabout;
   counter = counter % 5;
   delay(200);
@@ -37,4 +52,4 @@ void test(){
   if(counter == 0 || counter == 4){
     roundabout = roundabout * (-1);
   }
-}
+}*/
